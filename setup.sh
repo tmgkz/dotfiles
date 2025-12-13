34db86ea9f2864c800c6aef45a764aa09ffe65b5
tumugikaze <tumugikaze@gmail.com>
2025-12-13 13:25:41 +0900

    fix: Enable side-by-side view for delta in Git configuration

--- setup.sh
+++ setup.sh
@@ -297,6 +297,7 @@ if exists delta; then
     git config --global core.pager delta
     git config --global interactive.diffFilter "delta --color-only"
     git config --global delta.navigate true
+    git config --global delta.side-by-side true
     git config --global merge.conflictstyle diff3
     git config --global diff.colorMoved default
 fi
